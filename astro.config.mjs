a2df89390397de3bb8e222b4a0fecbac1edb099f
Yuki Shibata <gamobox@gmail.com>
2025-01-04 14:42:06 +0900

    feat: setup prettier and format all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gamobox.github.io',
-    base: '/',
-    integrations: [tailwind(), react()]
-});
+  site: 'https://gamobox.github.io',
+  base: '/',
+  integrations: [tailwind(), react()]
+})

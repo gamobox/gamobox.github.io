567a069be88799805cc1d13cd66ebadd7a7be92f
Yuki Shibata <gamobox@gmail.com>
2025-01-04 14:28:25 +0900

    feat: add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gamobox.github.io',
-  base: '/dist',
-  integrations: [tailwind(), react()]
+    site: 'https://gamobox.github.io',
+    base: '/',
+    integrations: [tailwind(), react()]
 });

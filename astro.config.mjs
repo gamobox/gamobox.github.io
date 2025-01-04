fb36871f72b4ef728ae22e94e0b85e5146300d5c
Yuki Shibata <gamobox@gmail.com>
2025-01-04 14:25:25 +0900

    feat: add tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gamobox.github.io',
-    base: '/dist'
+  site: 'https://gamobox.github.io',
+  base: '/dist',
+  integrations: [tailwind(), react()]
 });

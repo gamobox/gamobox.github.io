21d750b3e5279275582a86024dc4af94ba312ff7
Yuki Shibata <gamobox@gmail.com>
2025-01-05 19:36:25 +0900

    fix: CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gamobox.github.io',
+  site: 'https://nymorea.com',
   base: '/',
   integrations: [tailwind(), react(), mdx()]
 })
